a0138841d90f101e67990dcf7f8d1b8d3907c055
Simon Maher <simonmaher8307@hotmail.com>
2019-10-11 11:41:50 +0300

    added var variable

---
+++ index.js
@@ -0,0 +1,13 @@
+// VARIABLES    
+// VAR 
+// define 
+var number = 100
+console.log(number)
+// update 
+number = 200
+console.log(number)
+// redefine
+var number = 'orange' 
+console.log(number)
+
+// ************************** 
