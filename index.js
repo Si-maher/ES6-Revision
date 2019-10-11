ab909e0224c425b7b9824a6c53a8071b629098e6
Simon Maher <simonmaher8307@hotmail.com>
2019-10-11 13:13:02 +0300

    added block scope variable

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ console.log(fruit)
 // redefine 
 // let fruit = 'banana'
 // console.log(fruit)
-
+// ****************************
 // CONST 
 // define 
 const boilingPoint = 100
@@ -35,3 +35,39 @@ const person = {name:'Peter'}
 console.log(person)
 person.name = 'Simon'
 console.log(person)
+
+// ******************************
+
+// Local scope 
+
+function greeting() {
+var value = 'hello'
+console.log(value)
+}
+greeting()
+// console.log(value) 
+
+function greeting1 (){
+    let greet = 'hello'
+    console.log(greet);
+    
+}
+greeting1()
+
+// ******************* 
+
+// Block scope 
+
+var test = true
+var total = 1000
+if (test) {
+    var total = 'apples and pears'
+}
+console.log('Salary is ' + total);
+
+var test1 = true
+var amount = 2000
+if (test1) {
+    let amount = 'apples and pears'
+}
+console.log('Salary is ' + amount);
